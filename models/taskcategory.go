d948813390ca5ce5bdf3452e21a7fcc23e22dc61
suraj <hello.inphin@gmail.com>
2020-12-21 08:13:37 +1100

    add daos

--- models/taskcategory.go
+++ models/taskcategory.go
@@ -5,30 +5,30 @@ import (
 	"github.com/google/uuid"
 )
 
-type JobCategory struct {
+type TaskCategory struct {
 	ID          *[]byte `gorm:"Column:id" sql:"type:binary(16);not null"`
 	CategoryKey *int    `gorm:"Column:category_key" sql:"type:tinyint(4);not null"`
 	Name        *string `gorm:"Column:name" sql:"type:varchar(255);default:null"`
 }
 
-// NewJobCategory inits JobCategory struct
-func NewJobCategory() *JobCategory {
-	return &JobCategory{}
+// NewTaskCategory inits TaskCategory struct
+func NewTaskCategory() *TaskCategory {
+	return &TaskCategory{}
 }
 
 //TableName - returns name of the table
 //Implement mysql.GenericTable interface
-func (*JobCategory) TableName() string {
-	return "job_category"
+func (*TaskCategory) TableName() string {
+	return "Task_category"
 }
 
-func CreateJobCategory(name, parentID *string, categoryKey *int) (j *JobCategory, err error) {
+func CreateTaskCategory(name, parentID *string, categoryKey *int) (j *TaskCategory, err error) {
 	id, _ := uuid.New().MarshalBinary()
 	var p *[]byte
 	if parentID != nil {
 		*p = []byte(*parentID)
 	}
-	j = &JobCategory{
+	j = &TaskCategory{
 		ID:          &id,
 		Name:        name,
 		CategoryKey: categoryKey,
@@ -39,7 +39,7 @@ func CreateJobCategory(name, parentID *string, categoryKey *int) (j *JobCategory
 	return
 }
 
-func GetJobCategoryByID(id string) (*JobCategory, error) {
+func GetTaskCategoryByID(id string) (*TaskCategory, error) {
 	uuID, err := uuid.Parse(id)
 	if err != nil {
 		return nil, fmt.Errorf("unable to convert the string to UUID")
@@ -48,7 +48,7 @@ func GetJobCategoryByID(id string) (*JobCategory, error) {
 	if err != nil {
 		return nil, fmt.Errorf("unable to convert the UUID")
 	}
-	var j JobCategory
+	var j TaskCategory
 	j.ID = &idInBinary
 	if find := db.First(&j); find.Error != nil {
 		return nil, find.Error
